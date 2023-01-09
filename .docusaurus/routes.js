b0db9eddb43d43a54cd2e8a8dabcac142ccf6a3d
Bonjun Ku <ku96kbj@naver.com>
2023-01-09 21:24:43 +0900

    Add LeetCode 622.md

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -79,7 +79,7 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', 'db5'),
+    component: ComponentCreator('/docs', '3e1'),
     routes: [
       {
         path: '/docs/Problem Solving/about',
@@ -126,6 +126,12 @@ export default [
       {
         path: '/docs/Problem Solving/LeetCode/Best Time to Buy and Sell Stock',
         component: ComponentCreator('/docs/Problem Solving/LeetCode/Best Time to Buy and Sell Stock', '627'),
+        exact: true,
+        sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/Problem Solving/LeetCode/Design Circular Queue',
+        component: ComponentCreator('/docs/Problem Solving/LeetCode/Design Circular Queue', 'c4b'),
         exact: true,
         sidebar: "tutorialSidebar"
       },
